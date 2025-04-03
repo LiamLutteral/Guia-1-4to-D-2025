52af1f6dc1d19dda2fceb311faefba0b16c7a9bf
LiamLutteral <49008662@est.ort.edu.ar>
2025-04-03 10:40:23 -0300

    x

---
+++ Assets/Scripts/etacionamiento.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class etacionamiento : MonoBehaviour
+{
+    public int hora;
+    bool estaAbierto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (hora >= 0 || hora > 24)
+        {
+            Debug.Log("ha ingresado una hora incorrecta");
+            return;
+        }
+        else if (hora >= 10 && hora < 18) 
+        {
+            estaAbierto = true;
+        }else
+        {
+            estaAbierto = false;
+        }
+        Debug.Log("el estacionamiento esta abierto" + estaAbierto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
