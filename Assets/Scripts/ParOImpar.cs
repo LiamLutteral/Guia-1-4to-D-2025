9bbfa05f9f95815499e525d81c7225a0e4d4ad94
LiamLutteral <49008662@est.ort.edu.ar>
2025-04-03 10:16:43 -0300

    x

---
+++ Assets/Scripts/ParOImpar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParOImpar : MonoBehaviour
+{
+    public int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 > 0)
+        {
+            if(num1 % 2 == 0 )
+            {
+                Debug.Log("el numero es par");
+            }
+            else
+            {
+                Debug.Log("el numero es impar");
+            }
+        }
+        else
+        {
+            Debug.Log("el numero debe ser mayor a cero");
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
+
